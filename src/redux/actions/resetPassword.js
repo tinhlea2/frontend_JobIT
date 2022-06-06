94624b5888a4998552d3bd23e085f1a565b62242
Huong Le Lien <huongll@rising-stars.vn>
2022-06-06 20:41:34 +0700

    reset password function, register

--- src/redux/actions/resetPassword.js
+++ src/redux/actions/resetPassword.js
@@ -4,17 +4,15 @@ export function resetPassword(data, resolve = () => {}) {
   store.dispatch({
     type: types.RESET_PASSWORD,
   });
-  return fetch(
-    `${process.env.REACT_APP_API_URL}/auth/reset-password`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(data),
-    }
-  )
+
+  return fetch(`${process.env.REACT_APP_API_URL}/auth/reset-password`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  })
     .then((response) => response.json())
     .then((data) => {
       resolve(data);
