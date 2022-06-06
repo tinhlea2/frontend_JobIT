1f56e8a87968410f548a5aa899e79b20b3ea9f5b
Huong Le Lien <huongll@rising-stars.vn>
2022-06-06 20:39:09 +0700

    updated auth flow

--- src/redux/actions/changePassword.js
+++ src/redux/actions/changePassword.js
@@ -4,17 +4,15 @@ export function changePassword(data, resolve = () => {}) {
   store.dispatch({
     type: types.RESET_PASSWORD,
   });
-  return fetch(
-    `${process.env.REACT_APP_API_URL}/auth/change-password`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(data),
-    }
-  )
+
+  return fetch(`${process.env.REACT_APP_API_URL}/auth/change-password`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  })
     .then((response) => response.json())
     .then((data) => {
       resolve(data);
--- src/redux/actions/loginAdmin.js
+++ src/redux/actions/loginAdmin.js
@@ -1,20 +1,18 @@
 import * as types from "../constants";
 import store from "../store";
-export function loginAdmin(data, resolve = () => { }) {
+export function loginAdmin(data, resolve = () => {}) {
   store.dispatch({
     type: types.LOGIN_API,
   });
-  return fetch(
-    `${process.env.REACT_APP_API_URL}/admin/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(data),
-    }
-  )
+
+  return fetch(`${process.env.REACT_APP_API_URL}/admin/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  })
     .then((response) => response.json())
     .then((data) => {
       resolve(data);
