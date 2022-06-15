9176947d3c20dcaf9648c30212fc61e8e6618a52
Huong Le Lien <huongll@rising-stars.vn>
2022-06-15 22:31:50 +0700

    Fixed image show

--- src/App.js
+++ src/App.js
@@ -22,14 +22,6 @@ import ConfirmCode from "./components/pages/ConfirmCode";
 import ChangePassword from "./components/pages/ChangePassword";
 import DetailPost from "./components/common/DetailPost";
 import PageNotFound from "./components/pages/404-page";
-// Containers
-// const TheLayout = React.lazy(() => import("./containers/TheLayout"));
-// const LogInAdmin = React.lazy(() => import("./containers/LogInAdmin"));
-// Pages
-// const Login = React.lazy(() => import('./views/pages/login/Login'));
-// const Register = React.lazy(() => import('./views/pages/register/Register'));
-// const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-// const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
 
 function App() {
   return (
